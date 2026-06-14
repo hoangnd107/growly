60e8a18927cf0bb33c5e34759210926ec89e5a7c
Hoang Nguyen Duy <nduyhoang107@gmail.com>
2026-06-14 17:28:02 +0700

    Remove accidental WidgetBridge from growly

--- Starter/Widget/WidgetBridge.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-import WidgetKit
-
-@objc public class WidgetBridge: NSObject {
-    @objc public static func reloadAllTimelines() {
-        WidgetCenter.shared.reloadAllTimelines()
-    }
-}
